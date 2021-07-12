9501e8ba406445f08d26103b9032df19ebe314eb
Loi Tran <datloiboi@gmail.com>
2021-07-12 15:30:32 +0700

    Update app.js

--- app.js
+++ app.js
@@ -5,15 +5,13 @@ const logger = require("morgan");
 const cors = require("cors");
 require("dotenv").config();
 
+app.use(cors());
+
+app.use(logger("dev"));
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
-
-app.use(logger("dev"));
 
 app.use((req, res, next) => {
   const error = new Error("Resource Not Found");
@@ -21,12 +19,14 @@ app.use((req, res, next) => {
   next(error);
 });
 
+app.use(errorHandler);
+
 function errorHandler(err, req, res, next) {
   console.error(err);
   res.status(err.statusCode || 500);
   res.send(err.message);
 }
 
-app.use(errorHandler);
-
-app.use(cors());
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
